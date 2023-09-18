bf3b4110d6580fa125a380f0c1b759faf8ed23e4
AnitaOwen <anitaowen@pursuit.org>
2023-09-18 14:23:46 -0400

    created more functions for dinosoaurs and rooms for stretch goals

---
+++ src/stretch-goals.js
@@ -0,0 +1,73 @@
+const exampleDinosaurData = require("../data/dinosaurs");
+const exampleRoomData = require("../data/rooms");
+
+
+// find all the names of dinosaurs that only eat meat. 
+// returns an array of names of dinosaurs that eat meat.
+function getAllMeatEaters(dinosaurs){
+    const meatEaters = dinosaurs.filter(dinosaur => dinosaur.diet === 'carnivorous')
+    const meatEatersNames = meatEaters.map(dinosaur => dinosaur.name)
+    return meatEatersNames
+}
+console.log(getAllMeatEaters(dinosaurs))
+
+
+// find name and pronunciation of all dinosuars that lived during the Late Jurassic period. 
+// returns an array where each value is a name of a dinosaur and it's pronunciation
+function getDinosaursFromLateJurrasic(dinosaurs){
+    const lateJurrassicDinosaurs = dinosaurs.filter(dinosaur => dinosaur.period === "Late Jurassic")
+    const resultNamesArr = lateJurrassicDinosaurs.map(dinosaur => `${dinosaur.name} (${dinosaur.pronunciation})`)
+    return resultNamesArr
+}
+console.log(getDinosaursFromLateJurrasic(dinosaurs))
+
+
+// get all the names of herbivores that lived during a specified period
+// returns a string
+function getAllHerbivorousFromPeriod(dinosaurs, period){
+  const dinosaursInPeriod = dinosaurs.filter(dinosaur => dinosaur.period === period)
+  const herbivoresInPeriod = dinosaursInPeriod.filter(dinosaur => dinosaur.diet === "herbivorous")
+  const resultNames  = herbivoresInPeriod.map(dinosaur => dinosaur.name)
+  if(resultNames.length === 1){
+    return `${resultNames.toString()} is an herbivore that lived during the ${period} period.`
+  } else {
+    return `Herbivores that lived during the ${period} period:\n${resultNames.join(', ')}`
+  }
+}
+console.log(getAllHerbivorousFromPeriod(dinosaurs, "Early Cretaceous"))
+console.log(getAllHerbivorousFromPeriod(dinosaurs, "Late Cretaceous"))
+console.log(getAllHerbivorousFromPeriod(dinosaurs, "Early Jurassic"))
+console.log(getAllHerbivorousFromPeriod(dinosaurs, "Late Jurassic"))
+
+
+  // find the rooms that don't have any dinosaurs
+  // returns a string listings the names of rooms that don't have any dinosaurs
+  function roomsWithNoDinosaurs(rooms){
+    const noDinoRooms = rooms.filter(room => room.dinosaurs.length === 0)
+    const noDinoRoomNames = noDinoRooms.map(room => room.name)
+    return `These rooms do not have any dinosaurs: ${noDinoRoomNames.join(', ')}`
+}
+console.log(roomsWithNoDinosaurs(rooms))
+
+
+// find if a room has any required ticket permissions to enter
+// returns a string
+function getRequiredTicketTypeByRoomName(rooms, roomName){
+    const resultRoom = rooms.find(room => room.name === roomName)
+    if(!resultRoom){
+        return `There is no room called ${roomName}`
+    }
+    const ticketType = resultRoom.requiredTicketPermissions
+
+    if(resultRoom.requiredTicketPermissions.length === 0){
+        return `There are no ticket requirements to enter this room`
+    } else {
+        return `Tickets with ${ticketType} access required to enter this room`
+    }
+}
+console.log(getRequiredTicketTypeByRoomName(rooms, "Cabrera Hall"))
+console.log(getRequiredTicketTypeByRoomName(rooms, "Terrell Leon Lecture Room"))
+console.log(getRequiredTicketTypeByRoomName(rooms, "Blackwell Amphitheater"))
+console.log(getRequiredTicketTypeByRoomName(rooms, "Paxton Decker Terrace"))
+
+
