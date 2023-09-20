203bd5f2ee7152719f335f5e86ef8f2cf28ad971
juligarc91 <julissagarcia@pursuit.org>
2023-09-19 23:26:27 -0400

    Completed function getConnectedRoomNamesById() in file 02

--- src/02-room-details.js
+++ src/02-room-details.js
@@ -38,7 +38,7 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
     dinosaurId = dinoNameIdObj[dinosaurName]; // value of dinosaurId can be accessed with dinosaurName input as the key
   } else {
     return `Dinosaur with name '${dinosaurName}' cannot be found.` // error message if corresponding dino can't be accessed AT ALL (because input doesn't match referenced data)
-  } console.log (dinosaurId);
+  }
   // to get room names must iterate through rooms array
   for (const room of rooms) {
     if (room.dinosaurs.includes(dinosaurId)) { // room.dinosaurs is an array so .includes() can be used to find a specific value in this case dinosaurId (a string)
@@ -70,7 +70,35 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
       "Kit Hopkins Education Wing"
     ]
  */
-function getConnectedRoomNamesById(rooms, id) {}
+function getConnectedRoomNamesById(rooms, id) {
+  let roomWithInputId;
+  //find room with input id
+  for (let room of rooms) {
+    if (room.roomId === id) {
+      roomWithInputId = room; // room is an object that contains connectTo key I will need later
+      break; // once room is found break out of loop (can use .find() instead of for of loop but not sure how to implement)
+    }
+  } // outside for loop do return statement for error message incase id is incorrect or value not found
+  if (!roomWithInputId) { // if evaluates to true (bc incorrect id in input) will return error message
+    return "Room with ID of 'incorrect-id' could not be found.";
+  }
+  // Findiing names of connected rooms - nested for of loops (or for of loop with .find()) to access roomWithInputId.connectsTo array and find values that corresponds to connected rooms that connect to room dinosaur with corresponding id is located in
+  const connectedRoomNames = [];
+  for (let connectionId of roomWithInputId.connectsTo) { //searches through object we extracted containing room data that matches input id
+    let connectedRoomFound = false; //kind of like a starting counter but for booleans. Once room is found this will change to true and it'll stop looking. Or once it looks through everything and didn't find rooms it will exit loop and remain false
+    for (let room of rooms) {
+      if (room.roomId === connectionId) {
+        connectedRoomNames.push(room.name);
+        connectedRoomFound = true;
+        break; // exit loop once room is found
+      }
+    }
+    if (connectedRoomFound === false) {
+      return "Room with ID of 'incorrect-id' could not be found."; // string error msg if id input invalid in obj created roomWithInputId
+    }
+  }
+  return connectedRoomNames; // array of room names if id input valid
+};
 
 module.exports = {
   getRoomByDinosaurName,
